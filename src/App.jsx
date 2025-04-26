a1adc47dcc24d470aedf8c71b40bebfaa71ac4d6
Devesh-Kaushal <deveshkaushal004@gmail.com>
2025-04-26 14:54:31 +0530

    Deploy to gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import ProjectsPage from './components/ProjectsPage';
 import BlogPage from './components/BlogPage';
 import GalleryPage from './components/GalleryPage';
 import AdminPanel from './components/AdminPanel';
-import { initialData } from './data/initialData';
+import AboutPage from './components/AboutPage';
+import ResearchPage from './components/ResearchPage';
+import ContactPage from './components/ContactPage';
 
 export default function App() {
-  const [data, setData] = useState(initialData);
-
   return (
-    <BrowserRouter>
+    <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage projects={data.projects} blogs={data.blogs} gallery={data.gallery} />} />
-        <Route path="/projects" element={<ProjectsPage projects={data.projects} />} />
-        <Route path="/blog" element={<BlogPage blogs={data.blogs} />} />
-        <Route path="/gallery" element={<GalleryPage gallery={data.gallery} />} />
-        <Route path="/admin" element={<AdminPanel data={data} setData={setData} />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/research" element={<ResearchPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
-      <footer className="text-center text-gray-500 text-sm py-8 bg-black/50">
-        © 2025 DX Portfolio •
-        <a href="#" className="text-indigo-400 hover:underline">GitHub</a> •
-        <a href="#" className="text-indigo-400 hover:underline">LinkedIn</a> •
-        <a href="#" className="text-indigo-400 hover:underline">ResearchGate</a>
-      </footer>
-    </BrowserRouter>
+    </Router>
   );
 }
