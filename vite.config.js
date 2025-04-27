013ead253684e78e6ee5cc1b60df79777a4e11a1
Devesh-Kaushal <deveshkaushal004@gmail.com>
2025-04-27 23:59:41 +0530

    Fix

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Personal-Portfolio/', // Update with your GitHub repo name if different
-});
